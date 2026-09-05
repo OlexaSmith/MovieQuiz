9ac16bda7bb37b2ba43def1a5c839ab588534200
OlexaSmith <olexasmith@yandex.ru>
2026-09-05 10:47:41 +0300

    feat(network): загрузка фильмов из IMDb API sprint6

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,39 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Olya on 04.09.2026.
+//
+
+import Foundation
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_j4r66gt6") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+        func loadImage(from url: URL, handler: @escaping (Result<Data, Error>) -> Void){
+            networkClient.fetch(url: url, handler: handler)
+        }
+    }
+}
